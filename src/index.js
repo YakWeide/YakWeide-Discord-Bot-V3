22ca7b56270e8c1dc4aebd5cc39f3c4d2e9c997d
Tim Lisemer <timlisemer@gmail.com>
2023-03-21 16:25:01 +0100

    AuditLog

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
+const { Client, AuditLogEvent, Events } = require('discord.js');
 const { AudioPlayerStatus } = require('@discordjs/voice');
 
-const client = new Discord.Client({ intents: 8833 });
+const client = new Client({ intents: 3276799 });
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
@@ -24,120 +24,36 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async message => {
-    if (message.content === '!activate') {
+    if (message.content === '!enable') {
         goodbyeActive = true;
-    } else if (message.content === '!deactivate') {
+        console.log("Yak-Bot Enabled");
+        message.channel.send("Yak-Bot Enabled");
+    } else if (message.content === '!disable') {
         goodbyeActive = false;
+        message.channel.send("Yak-Bot Disabled");
+        console.log("Yak-Bot Disabled");
     }
 });
 
-client.on('voiceStateUpdate', async (oldState, newState) => {
-    await checkLog(oldState.member);
-
-    // if user leaves channel bot joins, plays Ciaoooo.mp3 and leaves
-    if (oldState.channelId !== null && newState.channelId === null && goodbyeActive) {
-        if (oldState.member.id !== client.user.id) {
-            const connection = await oldState.channel.join();
-            const player = connection.play('Ciaoooo.mp3');
-            player.on(AudioPlayerStatus.Idle, () => {
-                connection.disconnect();
-            });
-        }
-    }
+client.on('voiceStateUpdate', async (oldState) => {
+    await checkLog();
+    console.log("nach voiceStateUpdate");
 });
 
-async function checkLog(member) {
+async function checkLog() {
     const guild = client.guilds.cache.get(GUILD_ID);
-    let newlog = null;
-    let newdisconnect = null;
-    let newMove = null;
+    guild.fetchAuditLogs().then(logs => {
+        /*
+        logs.entries.forEach(entry => {
+            console.log(entry);
+            const { action, actionType, executorId, target, targetId } = entry;
+            console.log("action: " + action + " actionType: " + actionType + " executorId: " + executorId + " target: " + target + " targetId: " + targetId)
+        });
+         */
 
-    let i = 0;
-    const logs = await guild.fetchAuditLogs({limit: 100});
-    for (const entry of logs.entries) {
-        if (i === 0) {
-            newlog = entry;
-        }
-        const action = `${entry.action}`;
-        if (newdisconnect === null && action === 'MEMBER_DISCONNECT') {
-            newdisconnect = entry;
-        }
-        if (newMove === null && action === 'MEMBER_MOVE') {
-            newMove = entry;
-        }
-        if (newdisconnect !== null && newMove !== null) {
-            break;
-        }
-        i = i + 1;
-    }
-
-    if (previouslog === null) {
-        previouslog = newlog;
-        return;
-    }
-
-    if (previouslog.id !== newlog.id) {
-        previouslog = newlog;
-        await printLog(newlog, member);
-        return;
-    }
-
-    if (`${newlog.action}` === 'MEMBER_DISCONNECT') {
-        if (previousdisconnect === null) {
-            previousdisconnect = newlog;
-            await printLog(newlog, member);
-            return;
-        } else if (previousdisconnect.id !== newlog.id) {
-            previousdisconnect = newlog;
-            await printLog(newlog, member);
-            return;
-        } else {
-            console.log("previousdisconnect nein");
-        }
-    }
-
-    if (`${newlog.action}` === 'MEMBER_MOVE') {
-        if (previousmove === null) {
-            previousmove = newlog;
-            await printLog(newlog, member);
-        } else if (previousmove.id !== newlog.id) {
-            previousmove = newlog;
-            await printLog(newlog, member);
-        } else {
-            console.log("previousdisconnect nein");
-        }
-    }
-}
-
-async function printLog(entry, member) {
-    const channel = client.channels.cache.find(channel => channel.id === LOG_CHANNEL_ID);
-    const action = entry.action;
-
-    let message;
-    if (action === 'AuditLogAction.member_disconnect') {
-        if (member !== null) {
-            message = `${entry.user} disconnected ${member}`;
-        } else {
-            message = `${entry.user} disconnected a user`;
-        }
-    } else if (action === 'AuditLogAction.member_move') {
-        if (member !== null) {
-            message = `${entry.user} moved ${member}`;
-        } else {
-            message = `${entry.user} moved a user`;
-        }
-    } else if (action === 'AuditLogAction.unban') {
-        message = `${entry.user} unbanned ${entry.target}`;
-    } else if (action === 'AuditLogAction.ban') {
-        message = `${entry.user} banned ${entry.target}`;
-    } else if (action === 'AuditLogAction.member_role_update' || action === 'AuditLogAction.invite_create') {
-        return;
-    } else if (entry.target === null) {
-        message = `${entry.user} did ${action}`;
-    } else {
-        message = `${entry.user} did ${action} to ${entry.target}`;
-    }
-
-    console.log(message);
-    await channel.send(message);
+        const entry = logs.entries.first();
+        console.log(entry);
+        const { action, actionType, executorId, target, targetId } = entry;
+        console.log("action: " + action + " actionType: " + actionType + " executorId: " + executorId + " target: " + target + " targetId: " + targetId)
+    });
 }
